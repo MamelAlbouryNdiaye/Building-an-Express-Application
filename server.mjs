efc18b8b0e2a6f045c59343b52794012e8a7333e
Ndiaye Mamel Alboury <mamelalboury@gmail.com>
2025-09-04 07:24:57 +0200

    Implement an Express template engine. ejs

--- server.mjs
+++ server.mjs
@@ -1,8 +1,17 @@
 import express from 'express';
+import ejs from 'ejs'
 
 const app =  express()
 
 
-app.listen(3000, () =>{
+//register view engine
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) =>{
+    res.render('index', {title : 'HomePage'})
+})
+
+//port
+app.listen(4000, () =>{
     console.log('server is running ')
 })
