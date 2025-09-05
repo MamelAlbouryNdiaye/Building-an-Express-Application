e317546b38f8b7a9e37f666e5b51fcc564234773
Ndiaye Mamel Alboury <mamelalboury@gmail.com>
2025-09-05 02:06:56 +0200

    adding a middleware morgan

--- server.mjs
+++ server.mjs
@@ -1,13 +1,17 @@
 import express from 'express';
+import morgan from 'morgan';
 
 
 const app =  express()
 
 //Middleware to parse form data  
 app.use(express.urlencoded({extended: true}));
+//middleware morgan
+app.use(morgan('dev')); 
 
 //register view engine
 app.set('view engine', 'ejs');
+
 
 //routes
 app.get('/', (req, res) =>{
